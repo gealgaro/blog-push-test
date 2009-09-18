3a8a4fbe8246b6df77fe319836dca1311e0bd677
gealgaro <german.garcia.d@gmail.com>
2009-09-18 18:18:13 -0500

    CSS Changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_filter :login_required, :except => [:index, :show]
+  before_filter :login_required, :except => [:index, :show, :any]
   # GET /posts
   # GET /posts.xml
   def index
